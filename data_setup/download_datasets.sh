df1a458980939384de88f3e9f5d6ae90eb459869
Romouald Dombrovski <romouald.dombrovski@gmail.com>
2026-04-25 16:25:57 -0400

    Adding download of CUB_200 dataset to scripts

--- data_setup/download_datasets.sh
+++ data_setup/download_datasets.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
 # TODO: get datasets
 
+# CUB_200_2011 dataset
+echo "Getting CUB_200 dataset"
+wget https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz
+
+mkdir -p data/cub
+tar -xzf CUB_200_2011.tgz -C data/cub
+rm CUB_200_2011.tgz  

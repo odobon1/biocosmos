893b0b0e8a28f1040541fb7201f09675b8186082
Oliver Dobon <odobon3@gatech.edu>
2026-06-24 02:04:30 -0400

    cub phylo: key tree tips by class ID

--- preprocessing/cub/phylo.py
+++ preprocessing/cub/phylo.py
@@ -11,17 +11,19 @@ from utils.utils import paths, load_pickle, save_pickle
 import pdb
 
 
-def build_tree_cub() -> Tree:
+def build_tree_cub(class_data) -> Tree:
+    common_to_cid = {data["common_name"]: cid for cid, data in class_data.items()}
     tree = Phylo.read(paths["raw_tree"]["cub"], "newick")
     for tip in tree.get_terminals():
-        tip.name = tip.name[8:].lower().replace(" ", "_")
+        common_name = tip.name[8:].lower().replace(" ", "_")
+        tip.name = common_to_cid[common_name]
     return tree
 
 def main():
     print("Building CUB tree...")
 
-    tree = build_tree_cub()
     class_data = load_pickle(paths["metadata"]["cub"] / "class_data.pkl")
+    tree = build_tree_cub(class_data)
 
     tree_poly = augment_tree_with_polytomies(tree, class_data)
     tree_poly_pruned = prune_tree(tree_poly, class_data)
